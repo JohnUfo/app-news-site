715fa65e8a4503b688acc327aab43ca211b23b84
johnUfo <muydinovumidjon17@gmail.com>
2024-09-29 19:13:34 +0500

    admin can add roles

--- src/main/java/uz/muydinovs/appnewssite/controller/RoleController.java
+++ src/main/java/uz/muydinovs/appnewssite/controller/RoleController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class RoleController {
         this.roleService = roleService;
     }
 
+    @PreAuthorize(value = "hasAuthority('ADD_USER')")
     @PostMapping
     public HttpEntity<?> registerUser(@Valid @RequestBody RoleDto roleDto) {
         ApiResponse apiResponse = roleService.addRole(roleDto);
